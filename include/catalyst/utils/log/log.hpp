5f1dc2e352db22722c32b7785250031cbffe8009
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-17 11:05:08 -0700

    fix: new logging API

--- include/catalyst/utils/log/log.hpp
+++ include/catalyst/utils/log/log.hpp
@@ -40,6 +40,26 @@ public:
         logImpl(level, message);
     }
 
+    template <typename... ArgsT_T> void debug(std::format_string<ArgsT_T...> fmt, ArgsT_T &&...args) const {
+        std::string message = std::format(fmt, std::forward<ArgsT_T>(args)...);
+        logImpl(LogLevel::DEBUG, message);
+    }
+
+    template <typename... ArgsT_T> void info(std::format_string<ArgsT_T...> fmt, ArgsT_T &&...args) const {
+        std::string message = std::format(fmt, std::forward<ArgsT_T>(args)...);
+        logImpl(LogLevel::INFO, message);
+    }
+
+    template <typename... ArgsT_T> void warn(std::format_string<ArgsT_T...> fmt, ArgsT_T &&...args) const {
+        std::string message = std::format(fmt, std::forward<ArgsT_T>(args)...);
+        logImpl(LogLevel::WARN, message);
+    }
+
+    template <typename... ArgsT_T> void error(std::format_string<ArgsT_T...> fmt, ArgsT_T &&...args) const {
+        std::string message = std::format(fmt, std::forward<ArgsT_T>(args)...);
+        logImpl(LogLevel::ERROR, message);
+    }
+
     bool isOpen() const;
     void flush() const;
     void close() const;
