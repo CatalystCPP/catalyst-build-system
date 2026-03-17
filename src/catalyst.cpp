41452113568b69103af4ee5fd05a9cbc4ce0d199
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-17 02:54:38 -0700

    fix: logic error in workspace assignment to subcommands

--- src/catalyst.cpp
+++ src/catalyst.cpp
@@ -28,14 +28,17 @@ int main(int argc, char **argv) {
     if (should_return)
         return exit_code;
 
-    auto assign_ws_if = [&ctx](auto &res) {
-        if (res)
+    auto assign_ws_if = [&ctx](auto *subc, auto &res) {
+        if (subc && *subc) {
             res->workspace = ctx.workspace;
-        return static_cast<bool>(res);
+            return true;
+        }
+        return false;
     };
 
-    assign_ws_if(ctx.bench_res) || assign_ws_if(ctx.build_res) || assign_ws_if(ctx.clean_res) ||
-        assign_ws_if(ctx.fetch_res) || assign_ws_if(ctx.lock_res) || assign_ws_if(ctx.test_res);
+    assign_ws_if(ctx.bench_subc, ctx.bench_res) || assign_ws_if(ctx.build_subc, ctx.build_res) ||
+        assign_ws_if(ctx.clean_subc, ctx.clean_res) || assign_ws_if(ctx.fetch_subc, ctx.fetch_res) ||
+        assign_ws_if(ctx.lock_subc, ctx.lock_res) || assign_ws_if(ctx.test_subc, ctx.test_res);
 
     if (ctx.show_version) {
         std::println("{}", catalyst::CATALYST_VERSION);
