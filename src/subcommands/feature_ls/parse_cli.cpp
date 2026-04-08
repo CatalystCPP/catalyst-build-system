d287283f6f732e068cc0d43adcd8cf98e78e0d00
Siddharth Mohanty <neosiddharth@gmail.com>
2026-04-08 14:58:45 -0700

    fix: apply --inverse flag to feature_ls subc

--- src/subcommands/feature_ls/parse_cli.cpp
+++ src/subcommands/feature_ls/parse_cli.cpp
@@ -6,7 +6,7 @@ namespace catalyst::feature_ls {
 std::pair<CLI::App *, std::unique_ptr<Parse>> parse(CLI::App &app) {
     CLI::App *feature_ls = app.add_subcommand("feature-ls", "list all features across all profiles");
     auto ret = std::make_unique<Parse>();
-    app.add_flag("--inverse", ret->inverse, "list no-features as well");
+    feature_ls->add_flag("--inverse", ret->inverse, "list no-features as well");
     return {feature_ls, std::move(ret)};
 }
 } // namespace catalyst::feature_ls
