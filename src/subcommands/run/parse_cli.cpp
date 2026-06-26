8e1e339ac1efc445226285326b283e482352df70
Siddharth Mohanty <neosiddharth@gmail.com>
2026-06-26 01:07:09 -0700

    fix: run subc fix default parameters

--- src/subcommands/run/parse_cli.cpp
+++ src/subcommands/run/parse_cli.cpp
@@ -3,7 +3,7 @@
 auto catalyst::run::parse(CLI::App &app) -> std::pair<CLI::App *, std::unique_ptr<Parse>> {
     CLI::App *run = app.add_subcommand("run", "Run a built executable.");
     auto ret = std::make_unique<Parse>();
-    run->add_option("-p,--profiles", ret->profiles)->default_val("common"); // < temporary -q flag while we deprecate the old -p flag
-    run->add_option("-P,--params", ret->params)->default_val(std::vector<std::string>{});
+    run->add_option("-p,--profiles", ret->profiles)->default_val("common");
+    run->add_option("-P,--params", ret->params);
     return {run, std::move(ret)};
 }
