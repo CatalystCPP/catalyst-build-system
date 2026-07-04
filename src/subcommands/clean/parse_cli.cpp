64bb8a089af13741f2b681a62ecf0b3b0633f871
Siddharth Mohanty <neosiddharth@gmail.com>
2026-07-03 18:17:45 -0700

    fix: explicitly define default common profile

--- src/subcommands/clean/parse_cli.cpp
+++ src/subcommands/clean/parse_cli.cpp
@@ -3,7 +3,8 @@
 auto catalyst::clean::parse(CLI::App &app) -> std::pair<CLI::App *, std::unique_ptr<Parse>> {
     CLI::App *clean = app.add_subcommand("clean", "Clean artifacts.");
     auto ret = std::make_unique<Parse>();
-    clean->add_option("-p,--profiles", ret->profiles, "Profile composition to clean.");
+    clean->add_option("-p,--profiles", ret->profiles, "Profile composition to clean.")
+        ->default_val(std::vector<std::string>{"common"});
     clean->add_flag("-i,--intermediates", ret->intermediates, "Clean intermediate files only.");
     return {clean, std::move(ret)};
 }
--- src/subcommands/fetch/parse_cli.cpp
+++ src/subcommands/fetch/parse_cli.cpp
@@ -5,6 +5,7 @@
 auto catalyst::fetch::parse(CLI::App &app) -> std::pair<CLI::App *, std::unique_ptr<Parse>> {
     CLI::App *fetch = app.add_subcommand("fetch", "Fetch all dependencies for a profile composition.");
     auto ret = std::make_unique<Parse>();
-    fetch->add_option("-p,--profiles", ret->profiles);
+    fetch->add_option("-p,--profiles", ret->profiles, "Profile composition to fetch.")
+        ->default_val(std::vector<std::string>{"common"});
     return {fetch, std::move(ret)};
 }
